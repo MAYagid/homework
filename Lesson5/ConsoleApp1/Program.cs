b0e30087056cc74474a6c1658d2ebdd4929b7b9e
MAYagid <MAYagid@gmail.com>
2021-09-13 19:15:02 +0300

    HW 5 done

---
+++ Lesson5/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testfile = "testfile.txt";
+            Console.WriteLine("Введите строку: ");
+            string str1 = Console.ReadLine();
+            File.WriteAllText(testfile, str1);
+
+        }
+    }
+}
---
+++ Lesson5/Project4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Project4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = "C:\\";
+
+            string filename = "text.txt";
+            string n = "\n";
+            if (Directory.Exists(dirName))
+            {
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    File.AppendAllText(filename, s);
+                    File.AppendAllText(filename, n);
+                }
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    File.AppendAllText(filename, s);
+                    File.AppendAllText(filename, n);
+                }
+            }
+        }
+    }
+}
+
